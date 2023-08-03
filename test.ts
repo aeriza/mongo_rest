6bd090a731d484345e7b272bde5e68a564fbda8c
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-08-03 18:46:40 +0700

    create test file

---
+++ test.ts
@@ -0,0 +1,19 @@
+import {
+  Client
+} from "./mod.ts";
+
+const client = new Client({
+  cluster: "Cluster0",
+  baseUrl: Deno.env.get("MONGO_API_URL"),
+  apiKey: Deno.env.get("MONGO_API_KEY")
+});
+
+const document = await client.findOne({
+  db: "test",
+  collection: "coll",
+  filter: {
+    _id: 1
+  }
+});
+
+console.log(document);
