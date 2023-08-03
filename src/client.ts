1b50ac8a9bb4880983d25086cb2ad1cda4a6eb79
ntan <140772578+aeriza@users.noreply.github.com>
2023-08-03 17:12:30 +0700

    add defaultDatabase property to Client

--- src/client.ts
+++ src/client.ts
@@ -13,10 +13,13 @@ export class Client {
   #apiKey: string;
   #baseUrl: string;
   #cluster: string;
+  defaultDatabase?: string;
   
   constructor(options: ClientOptions) {
     this.#apiKey = options.apiKey;
     this.#baseUrl = options.baseUrl instanceof URL ? options.baseUrl.toString() : options.baseUrl;
     this.#cluster = options.cluster;
+
+    if ("defaultDatabase" in options) this.defaultDatabase = options.defaultDatabase;
   }
 }
