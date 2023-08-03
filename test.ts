1e8b5b2ead8d5e46dbc059dfebcdbc4063341c93
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-08-03 19:03:59 +0700

    ignore nullable env.get return value

--- test.ts
+++ test.ts
@@ -4,8 +4,8 @@ import {
 
 const client = new Client({
   cluster: "Cluster0",
-  baseUrl: Deno.env.get("MONGO_API_URL"),
-  apiKey: Deno.env.get("MONGO_API_KEY")
+  baseUrl: Deno.env.get("MONGO_API_URL")!,
+  apiKey: Deno.env.get("MONGO_API_KEY")!
 });
 
 const document = await client.findOne({
