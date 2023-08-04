46fee5c3fa2d1aeaae19a8cabdd25bcdb649da06
ntan <140772578+aeriza@users.noreply.github.com>
2023-08-04 08:54:05 +0700

    add generic type to collection class

--- src/collection.ts
+++ src/collection.ts
@@ -1,13 +1,16 @@
 import type {
   Client
 } from "./client.ts";
+import type {
+  Document
+} from "../deps.ts";
 
 export interface CreateCollectionOptions {
   dbName: string;
   name: string;
 }
 
-export class Collection {
+export class Collection<T extends Document> {
   #client: Client;
   #dbName: string;
   #name: string;
