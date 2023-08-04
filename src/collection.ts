64d2744b9e15310d5a85ad0977ee0db35601075c
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-08-04 19:36:30 +0700

    debyg response

--- src/collection.ts
+++ src/collection.ts
@@ -56,6 +56,7 @@ export class Collection<T extends Document> {
     console.log(request);
     
     const response = await request.json();
+    console.log(response);
     
     if ([400, 401].includes(request.status)) {
       throw new Error(response.error)
