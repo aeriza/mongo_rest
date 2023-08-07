d1b2f4e2817a5f8754f86c332f78a1ad0bd68abf
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-08-07 14:54:57 +0700

    create insertOne function

--- src/collection.ts
+++ src/collection.ts
@@ -83,4 +83,10 @@ export class Collection<T extends Document> {
 
     return data.insertedIds;
   }
+  
+  async insertOne(doc: T:): Promise<Required<InsertDocument<T>> | ObjectId> {
+    const data = await this.insertMany([doc]);
+
+    return data.insertedIds[0];
+  }
 }
