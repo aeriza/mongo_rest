70e5031e69995f96db1502e99b0540a6c7df1f22
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-08-16 23:02:01 +0700

    add some interfaces to types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,6 +2,26 @@ import type {
   Document
 } from "../deps.ts";
 
+/** Preset data for API requests to mongo servers */
+export interface RequestBody {
+  /** Your mongo atlas cluster name */
+  dataSource: string;
+  /** Your database name */
+  database: string;
+  /** Your collection name */
+  collection: string;
+}
+
+/**
+ * Options for collection creation
+ */
+export interface CollectionOptions {
+  /** Database name you want to manage */
+  dbName: string;
+  /** The name of the collection in the database that you want to manage */
+  name: string;
+}
+
 /** Options for find documents from collection */
 export interface FindOptions {
   limit?: number;
